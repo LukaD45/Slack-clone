810f1ecd608dd21271048db91d845456204925af
LukaD45 <luka.dusko44@gmail.com>
2024-09-27 10:04:16 +0200

    Current user API

---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Loader } from "lucide-react";
+
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { useCurrentUser } from "../hooks/use-current-user";
+
+export const UserButton = () => {
+  const { data, isLoading } = useCurrentUser();
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+  if (data === null) {
+    return null;
+  }
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage />
+          <AvatarFallback></AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem></DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
