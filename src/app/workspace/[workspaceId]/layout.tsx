3b06f89f5484f5cfc04cd3335263952d10bee0fb
LukaD45 <luka.dusko44@gmail.com>
2024-10-03 15:13:01 +0200

    Convex members route

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,17 +1,39 @@
 "use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
-interface WorkspaceIdLayout {
+interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
 }
 
-const WorkspaceIdLayout = ({ children }: WorkspaceIdLayout) => {
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
   return (
     <div className="h-full">
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
-        <Sidebar /> {children}
+        <Sidebar />
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="ca-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5e2c5f]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
