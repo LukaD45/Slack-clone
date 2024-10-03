e585667b5bb69d230ebaddfa98d372c8cd86a49b
LukaD45 <luka.dusko44@gmail.com>
2024-10-03 13:10:30 +0200

    Sidebar-button component

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,10 +1,16 @@
 import { UserButton } from "@/features/auth/components/user-button";
 import { WorkspaceSwitcher } from "./workspase-switcher";
+import { SidebarButton } from "./sidebar-button";
+import { Bell, Home, MessagesSquare, MoreHorizontal } from "lucide-react";
 
 export const Sidebar = () => {
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-9 pb-4px">
       <WorkspaceSwitcher />
+      <SidebarButton icon={Home} label="Home" isActive />
+      <SidebarButton icon={MessagesSquare} label="DMs" />
+      <SidebarButton icon={Bell} label="Activity" />
+      <SidebarButton icon={MoreHorizontal} label="More" />
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
       </div>
