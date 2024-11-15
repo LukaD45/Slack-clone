0fe39ed55a0be936e6b8d88749da12835af2e6d5
LukaD45 <luka.dusko44@gmail.com>
2024-11-15 08:47:23 +0100

    Renderer compoinent

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,5 +1,6 @@
 import { format, isToday, isYesterday } from "date-fns";
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
+import { Message } from "./message";
 
 interface MessageListProps {
   memberName?: string;
@@ -55,7 +56,28 @@ export const MessageList = ({
             </span>
           </div>
           {messages.map((message, index) => {
-            return <div key={message._id}>{JSON.stringify(message)}</div>;
+            return (
+              <Message
+                key={message._id}
+                id={message._id}
+                memberId={message.memberId}
+                authorImage={message.user.image}
+                authorName={message.user.name}
+                isAuthor={false}
+                reactions={message.reactions}
+                body={message.body}
+                image={message.image}
+                updatedAt={message.updatedAt}
+                createdAt={message._creationTime}
+                isEditing={false}
+                setEditingId={() => {}}
+                isCompact={false}
+                hideThreadButton={false}
+                threadCount={message.threadCount}
+                threadImage={message.threadImage}
+                threadTimestamp={message.threadTimestamp}
+              />
+            );
           })}
         </div>
       ))}
