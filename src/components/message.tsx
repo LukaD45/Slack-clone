fbb41e53a7bc9a516fcf7e61eeb940526293528f
LukaD45 <luka.dusko44@gmail.com>
2024-11-26 07:46:02 +0100

    Fixed margins while displaying messages and relocated Avatar classNames

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -62,8 +62,13 @@ export const Message = ({
               {format(new Date(createdAt), "hh:mm")}
             </button>
           </Hint>
+          <div className="flex flex-col w-full">
+            <Renderer value={body} />
+            {updatedAt ? (
+              <span className="text-xs text-muted-foreground">(edited)</span>
+            ) : null}
+          </div>
         </div>
-        <Renderer value={body} />
       </div>
     );
   }
@@ -74,11 +79,9 @@ export const Message = ({
     <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative">
       <div className="flex items-start gap-2">
         <button>
-          <Avatar className="size-5 rounded-md mr-1">
-            <AvatarImage className="rounded-md" src={authorImage} />
-            <AvatarFallback className="bg-sky-500 text-white rounded-md text-xs">
-              {avatarFallback}
-            </AvatarFallback>
+          <Avatar>
+            <AvatarImage src={authorImage} />
+            <AvatarFallback>{avatarFallback}</AvatarFallback>
           </Avatar>
         </button>
         <div className="flex flex-col w-full overflow-hidden">
@@ -97,6 +100,9 @@ export const Message = ({
             </Hint>
           </div>
           <Renderer value={body} />
+          {updatedAt ? (
+            <span className="text-xs text-muted-foreground">Edited</span>
+          ) : null}
         </div>
       </div>
     </div>
