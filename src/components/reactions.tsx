af21649f5ed6c0ce15ab939c26d87033824586cd
LukaD45 <luka.dusko44@gmail.com>
2024-11-27 22:23:56 +0100

    reaction count

--- src/components/reactions.tsx
+++ src/components/reactions.tsx
@@ -1,6 +1,9 @@
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { Doc, Id } from "../../convex/_generated/dataModel";
 import { useCurrentMember } from "@/features/members/api/use-current-member";
+
+import { cn } from "@/lib/utils";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+import { Doc, Id } from "../../convex/_generated/dataModel";
 
 interface ReactionsProps {
   data: Array<
@@ -25,7 +28,23 @@ export const Reactions = ({ data, onChange }: ReactionsProps) => {
   return (
     <div className="flex items-center gap-1 mt-1 mb-1">
       {data.map((reaction) => (
-        <button>{reaction.value}</button>
+        <button
+          className={cn(
+            "h-6 px-2 rounded-full bg-slate-200/70 border border-transparent text-slate-800 flex items-center gap-x-1",
+            reaction.memberIds.includes(currentMemberId) &&
+              "bg-blue-100/70 border-blue-500 text-white"
+          )}
+        >
+          {reaction.value}
+          <span
+            className={cn(
+              "text-xs font-semibold text-muted-foreground",
+              reaction.memberIds.includes(currentMemberId) && "text-blue-500"
+            )}
+          >
+            {reaction.count}
+          </span>
+        </button>
       ))}
     </div>
   );
