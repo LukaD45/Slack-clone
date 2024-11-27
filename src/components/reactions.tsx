5cd484be2c505b04f3e0d907d80210b2e410acbc
LukaD45 <luka.dusko44@gmail.com>
2024-11-27 22:28:53 +0100

    adding a reaction by clicking on someones reaction and hint

--- src/components/reactions.tsx
+++ src/components/reactions.tsx
@@ -1,6 +1,7 @@
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 
 import { cn } from "@/lib/utils";
+import { Hint } from "./hint";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 import { Doc, Id } from "../../convex/_generated/dataModel";
@@ -28,23 +29,29 @@ export const Reactions = ({ data, onChange }: ReactionsProps) => {
   return (
     <div className="flex items-center gap-1 mt-1 mb-1">
       {data.map((reaction) => (
-        <button
-          className={cn(
-            "h-6 px-2 rounded-full bg-slate-200/70 border border-transparent text-slate-800 flex items-center gap-x-1",
-            reaction.memberIds.includes(currentMemberId) &&
-              "bg-blue-100/70 border-blue-500 text-white"
-          )}
+        <Hint
+          key={reaction._id}
+          label={`${reaction.count} ${reaction.count === 1 ? "person" : "people"} reacted with ${reaction.value}`}
         >
-          {reaction.value}
-          <span
+          <button
+            onClick={() => onChange(reaction.value)}
             className={cn(
-              "text-xs font-semibold text-muted-foreground",
-              reaction.memberIds.includes(currentMemberId) && "text-blue-500"
+              "h-6 px-2 rounded-full bg-slate-200/70 border border-transparent text-slate-800 flex items-center gap-x-1",
+              reaction.memberIds.includes(currentMemberId) &&
+                "bg-blue-100/70 border-blue-500 text-white"
             )}
           >
-            {reaction.count}
-          </span>
-        </button>
+            {reaction.value}
+            <span
+              className={cn(
+                "text-xs font-semibold text-muted-foreground",
+                reaction.memberIds.includes(currentMemberId) && "text-blue-500"
+              )}
+            >
+              {reaction.count}
+            </span>
+          </button>
+        </Hint>
       ))}
     </div>
   );
