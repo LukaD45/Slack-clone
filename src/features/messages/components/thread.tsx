cb3b957f46be25ef78ef845cbc4ac0ae4cb8d637
LukaD45 <luka.dusko44@gmail.com>
2024-11-28 13:50:14 +0100

    getById hook

--- src/features/messages/components/thread.tsx
+++ src/features/messages/components/thread.tsx
@@ -10,7 +10,7 @@ interface ThreadProps {
 export const Thread = ({ messageId, onClose }: ThreadProps) => {
   return (
     <div className="h-full flex flex-col">
-      <div className="flex justify-between items-center p-4 border-b">
+      <div className="flex justify-between items-center px-4 h-[49px] border-b">
         <p className="text-lg font-bold">Thread</p>
         <Button onClick={onClose} size="iconSm" variant="ghost">
           <XIcon className="size-5 stroke-[1.5]" />
