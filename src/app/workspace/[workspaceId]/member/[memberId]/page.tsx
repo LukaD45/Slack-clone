e8437265514a61106a557c1fc164ffabc3cf8c43
LukaD45 <luka.dusko44@gmail.com>
2024-11-28 23:03:31 +0100

    conversations.ts createOrGet API endpoint

--- src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
+++ src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import { useMemberId } from "@/hooks/use-member-id";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 const MemberIdPage = () => {
   const workspaceId = useWorkspaceId();
-
-  return <div>Memebr Id Page</div>;
+  const memberId = useMemberId();
+  return <div>{JSON.stringify({ memberId, workspaceId })}</div>;
 };
 
 export default MemberIdPage;
