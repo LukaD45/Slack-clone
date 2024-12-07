21ad60758e4cfabcbd433a1c6e2f1bc7bce9a99e
LukaD45 <luka.dusko44@gmail.com>
2024-12-07 21:36:39 +0100

    conversation.tsx usePanel for openinguser profile

--- src/app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
+++ src/app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -1,10 +1,10 @@
 import { Loader } from "lucide-react";
 
 import { useMemberId } from "@/hooks/use-member-id";
-
 import { useGetMember } from "@/features/members/api/use-get-member";
 import { useGetMessages } from "@/features/messages/api/use-get-messages";
 
+import { usePanel } from "@/hooks/use-panel";
 import { MessageList } from "@/components/message-list";
 
 import { Header } from "./header";
@@ -17,6 +17,7 @@ interface ConversationProps {
 }
 
 export const Conversation = ({ id }: ConversationProps) => {
+  const { onOpenProfile } = usePanel();
   const memberId = useMemberId();
 
   const { data: member, isLoading: memberLoading } = useGetMember({
@@ -37,7 +38,7 @@ export const Conversation = ({ id }: ConversationProps) => {
       <Header
         memberName={member?.user.name}
         memberImage={member?.user.image}
-        onClick={() => {}}
+        onClick={() => onOpenProfile(memberId)}
       />
       <MessageList
         data={results}
