f35d92918d61df31dce518e104329833f26d2cf5
LukaD45 <luka.dusko44@gmail.com>
2024-12-07 20:32:54 +0100

    different functionalities when looking at own/other member profile

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -1,11 +1,21 @@
-import { AlertTriangle, Loader, MailIcon, XIcon } from "lucide-react";
+import {
+  AlertTriangle,
+  ChevronDownIcon,
+  Loader,
+  MailIcon,
+  XIcon,
+} from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useGetMember } from "../api/use-get-member";
+import { useUpdateMember } from "../api/use-update-member";
+import { useRemoveMember } from "../api/use-remove-member";
+import { useCurrentMember } from "../api/use-current-member";
 
 import { Id } from "../../../../convex/_generated/dataModel";
 
@@ -15,11 +25,20 @@ interface ProfileProps {
 }
 
 export const Profile = ({ memberId, onClose }: ProfileProps) => {
+  const workspaceId = useWorkspaceId();
+
+  const { data: currentMember, isLoading: isLoadingCurrentMmeber } =
+    useCurrentMember({ workspaceId });
   const { data: member, isLoading: isLoadingMember } = useGetMember({
     id: memberId,
   });
 
-  if (isLoadingMember) {
+  const { mutate: updateMmeber, isPending: isUpdateingMember } =
+    useUpdateMember();
+  const { mutate: removeMember, isPending: isRemovingMember } =
+    useRemoveMember();
+
+  if (isLoadingMember || isLoadingCurrentMmeber) {
     return (
       <div className="h-full flex flex-col">
         <div className="flex justify-between items-center px-4 h-[49px] border-b">
@@ -71,6 +90,24 @@ export const Profile = ({ memberId, onClose }: ProfileProps) => {
       </div>
       <div className="flex flex-col p-4">
         <p className="text-xl font-bold">{member.user.name}</p>
+        {currentMember?.role === "admin" && currentMember?._id !== memberId ? (
+          <div className="flex items-center gap-2 mt-4">
+            <Button variant="outline" className="w-full capitalize">
+              {member.role}
+              <ChevronDownIcon className="size-4 ml-2" />
+            </Button>
+            <Button variant="outline" className="w-full">
+              Remove
+            </Button>
+          </div>
+        ) : currentMember?._id === memberId &&
+          currentMember.role !== "admin" ? (
+          <div className="mt-4">
+            <Button variant="outline" className="w-full">
+              Leave
+            </Button>
+          </div>
+        ) : null}
       </div>
       <Separator />
       <div className="flex flex-col p-4">
